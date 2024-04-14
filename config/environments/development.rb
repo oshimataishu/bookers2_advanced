036e4755f29e928461f7c8e9bf9e7d7730dda996
oshimataishu <oshima.taishu.infratop@gmail.com>
2024-04-14 17:37:57 +0900

    finished installing password-changing feature

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
